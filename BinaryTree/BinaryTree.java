70faeabbd6e123a50efccae95ea6429657326c07
IamShiro <42015013+IamShiro@users.noreply.github.com>
2020-02-20 12:20:28 -0300

    BinaryTree.java

---
+++ BinaryTree/BinaryTree.java
@@ -0,0 +1,116 @@
+
+public class BinaryTree{
+	
+	Node root;
+	Node cur_node;
+	Node variable;
+	
+	public BinaryTree(Node root){
+		
+		this.root = root;
+		
+	}
+	
+	public void Insert(Node newNode) {
+		
+		if( root == null) {
+			
+			this.root = newNode;
+		}
+		else {
+			
+			this.cur_node = root;
+			
+			while(cur_node != null) {
+				
+				this.variable = cur_node;
+				
+				if (newNode.getValue() > cur_node.getValue()) {
+					
+						cur_node = cur_node.getRight();
+						
+				}else {
+					
+					cur_node = cur_node.getLeft();
+					
+					}
+				if (newNode.getValue() > variable.getValue()) {
+					
+					variable.setRight(newNode);
+					
+					} 
+				else {
+					
+					variable.setLeft(newNode);
+					
+					}
+				
+				newNode.setFather(variable);
+				
+				}
+			}
+		}
+	}
+
+
+class Node{
+	
+	Node right, left, father;
+	int value;
+	
+	Node(int value){
+		
+		this.right = null;
+		this.left  = null;
+		this.father = null;
+		this.value = value;
+		
+		}
+	
+	int getValue(){
+		
+		return value;
+		
+		}
+	
+	Node getRight() {
+		
+		return right;
+		
+		}
+	
+	void setRight(Node r) {
+		
+		this.right = r;
+		
+		}
+	
+	Node getLeft() {
+		
+		return left;
+		
+		}
+	
+	void setLeft(Node l) {
+		
+		this.left = l;
+		
+		}
+	
+	Node getFather() {
+		
+		return father;
+		
+		}
+	
+	void setFather(Node f) {
+		
+		this.father = f;
+		
+		}
+}
+
+
+
+
+

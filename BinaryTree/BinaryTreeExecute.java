eeec89ab636c1d474bff5b0d097f9163bb9cda45
IamShiro <42015013+IamShiro@users.noreply.github.com>
2020-02-20 13:48:00 -0300

    Update BinaryTreeExecute.java

--- BinaryTree/BinaryTreeExecute.java
+++ BinaryTree/BinaryTreeExecute.java
@@ -4,8 +4,14 @@ public class BinaryTreeTesting {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Node root = new Node(0);
-		Node son  = new Node(1);
+		Node root = new Node(5);
+		Node eight  = new Node(8);
+		Node three = new Node(3);
+		Node four = new Node(4);
+		Node two = new Node(2);
+		Node six = new Node(6);
+		Node seven = new Node(7);
+	
 		//System.out.println(root.getValue());
 		
 		BinaryTree tree = new BinaryTree(null);
@@ -13,10 +19,17 @@ public class BinaryTreeTesting {
 		//System.out.println(tree.root.getValue());
 		
 		tree.Insert(root);
-		tree.Insert(son);
+		tree.Insert(seven);
+		tree.Insert(three);
+		tree.Insert(four);
+		tree.Insert(two);
+		tree.Insert(six);
+		tree.Insert(eight);
 		
-		System.out.println(tree.root.getValue());
-		System.out.println(tree.root.right.getValue());
+		
+		System.out.println(tree.root.getValue()); //Value of the root
+		System.out.println(tree.root.right.right.getValue()); //Minimum value
+		System.out.println(tree.root.left.left.getValue()); //Maximum value
 	}
 
 }
